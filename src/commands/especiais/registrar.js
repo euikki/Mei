c86d4493a2ffc7ec5cdfa3f4749c002555b9426f
toichin-kk <lucasrafael20mal@gmail.com>
2024-02-06 21:53:10 -0300

    refactor: replaces specific request using the 'client' object

--- src/commands/especiais/registrar.js
+++ src/commands/especiais/registrar.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const User = require('../../mongoDB/schemas/userschema');
 const insigniaa = require('../../constants/insignias.json');
-const { FormatEmoji } = require("../../functions");
 
 module.exports = {
   name: "registrar",
@@ -12,11 +11,11 @@ module.exports = {
     let userData = await User.findOne({ _id: message.author.id });
 
     if (userData) {
-      return message.reply(FormatEmoji('> - {e:erro} Você já está registrado no meu **banco de dados**!'));
+      return message.reply(client.FormatEmoji('> - {e:erro} Você já está registrado no meu **banco de dados**!'));
     }
 
    
-await message.reply({ content: FormatEmoji(`> - {e:lista} \`${message.author.username}\` você completou seu registro e com isso ganhou a **(${insigniaa.insig.registro}) Insígnia de aventureiro**!`)}); 
+await message.reply({ content: client.FormatEmoji(`> - {e:lista} \`${message.author.username}\` você completou seu registro e com isso ganhou a **(${insigniaa.insig.registro}) Insígnia de aventureiro**!`)}); 
 
 xana = new User({ _id: message.author.id, insignia: insigniaa.insig.registro});
 await xana.save();
