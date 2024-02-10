5cee81e34fedd4adda9e548fe3301bfe8f32bc54
toichin-kk <lucasrafael20mal@gmail.com>
2024-02-10 19:20:51 -0300

    feat: modifies button and author checking logic

--- src/events/interaction.js
+++ src/events/interaction.js
@@ -4,14 +4,14 @@ module["exports"] = {
   
         if (interaction.isButton()) {
             const args = (interaction.customId.match(/\[([^\]]*)\]/) || [, ''])[1].split(',').map(param => param.trim());
-
+        
             const Button = args.length > 0 ? client.components.get(args[0]) : client.components.get(interaction.customId);
-            if (Button.authorOnly && interaction.user.id !== args[1])
-            return interaction.deferUpdate();
-
+            if (!Button || (Button.authorOnly && interaction.user.id !== args[1]))
+                return interaction.deferUpdate();
+        
             args.shift()
             if (Button) Button.run(client, interaction, args);
-        }
+        }        
 
     }
 }
