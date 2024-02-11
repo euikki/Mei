b0eee2abf5fb58648163c111c71096b497f6f140
toichin-kk <lucasrafael20mal@gmail.com>
2024-02-11 13:20:17 -0300

    feat(schemas): add additional fields to the server schema

--- src/mongoDB/schemas/guildschema.js
+++ src/mongoDB/schemas/guildschema.js
@@ -2,7 +2,15 @@ const { Schema, model } = require('mongoose');
 
 const ServerSchema = new Schema({
     _id: { type: String, required: true },
-    entrada: { type: Boolean, default: false },
+    entrada: { 
+        type: Boolean, 
+        default: false,
+        title: { type: String, default: null },
+        description: { type: String, default: null },
+        thumbnail: { type: String, default: null },
+        image: { type: String, default: null },
+        color: { type: String, default: null }
+    },
     canal: { type: String, default: null }
 });
 
