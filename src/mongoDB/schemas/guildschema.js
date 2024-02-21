f1caad8d0829094850d8282098735109f4ffb078
toichin-kk <lucasrafael20mal@gmail.com>
2024-02-20 23:54:35 -0300

    refactor: simplifies server schema structure

--- src/mongoDB/schemas/guildschema.js
+++ src/mongoDB/schemas/guildschema.js
@@ -2,15 +2,12 @@ const { Schema, model } = require('mongoose');
 
 const ServerSchema = new Schema({
     _id: { type: String, required: true },
-    entrada: { 
-        type: Boolean, 
-        default: false,
-        title: { type: String, default: null },
-        description: { type: String, default: null },
-        thumbnail: { type: String, default: null },
-        image: { type: String, default: null },
-        color: { type: String, default: null }
-    },
+    entrada: { type: Boolean, default: false },
+    title: { type: String, default: null },
+    description: { type: String, default: null },
+    thumbnail: { type: String, default: null },
+    image: { type: String, default: null },
+    color: { type: String, default: null },
     canal: { type: String, default: null }
 });
 
