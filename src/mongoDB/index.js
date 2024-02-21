ff9ee643382a245aa5694a5553cea51952ddddf0
toichin-kk <lucasrafael20mal@gmail.com>
2024-02-20 22:58:59 -0300

    chore: replace npm colors with strcolorize

--- src/mongoDB/index.js
+++ src/mongoDB/index.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
+const colorize = require('strcolorize');
 require('dotenv').config();
-require('colors');
 
 const user = require('./schemas/userschema');
 
 mongoose.connect(process.env.database)
     .then(() => {
-        console.log(`${'Database'.cyan.bold} carregada com sucesso!`);
+        console.log(colorize(`[${'Database'}](yellow bold) carregada com sucesso!`));
     })
     .catch((error) => {
         console.error('Ocorreu um erro durante a conexão com o banco de dados:', error);
