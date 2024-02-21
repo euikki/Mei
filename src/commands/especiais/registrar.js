136f815b3faf974cd17d0429af65c480e7e8348d
toichin-kk <lucasrafael20mal@gmail.com>
2024-02-21 00:14:06 -0300

    feat: update registry command and add gift button

--- src/commands/especiais/registrar.js
+++ src/commands/especiais/registrar.js
@@ -1,22 +1,26 @@
 const Discord = require("discord.js");
 const User = require('../../mongoDB/schemas/userschema');
-const insigniaa = require('../../constants/insignias.json');
+const Mbutton = require("../../functions/buttonBuilder")
 
 module.exports = {
   name: "registrar",
   description: "🌹 Atravesse o portal para utilizar meus comandos.",
   category: "special",
 
-  run: async (client, message, args) => {
-    let userData = await User.findOne({ _id: message.author.id });
+run: async (client, message, args) => {
+  let userData = await User.findOne({ _id: message.author.id });
 
-    if (userData) {
-      return message.reply(client.FormatEmoji('> - {e:erro} Você já está registrado no meu **banco de dados**!'));
-    }
+  if (userData) {
+    return message.reply(client.FormatEmoji('> - {e:erro} Você já está registrado no meu **banco de dados**!'));
+  }
 
-   
-await message.reply({ content: client.FormatEmoji(`> - {e:lista} \`${message.author.username}\` você completou seu registro e com isso ganhou a **(${insigniaa.insig.registro}) Insígnia de aventureiro**!`)}); 
 
-xana = new User({ _id: message.author.id, insignia: insigniaa.insig.registro});
-await xana.save();
+const button = Mbutton([
+  {label: "Ver presente", emoji: client.FormatEmoji("{e:gift}"), customId: `[gift, ${message.author.id}]`, style: "Secondary"}
+])
+
+await message.reply({ content: client.FormatEmoji(`> {e:lista} \`${message.author.username}\` Parabéns por se registrar! com isso você acaba de ganhar **2 presentes**.\n > - Aperte no botão abaixo para visualizar seus presentes.`), components: [button] });
+
+user = new User({ _id: message.author.id});
+await user.save();
 }}
