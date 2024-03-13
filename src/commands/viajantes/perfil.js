b1cd5fdfe76326e4aeae1ab22f4879835aec3821
toichin-kk <lucasrafael20mal@gmail.com>
2024-03-12 21:40:29 -0300

    feat: add new visual styling and profile information

--- src/commands/viajantes/perfil.js
+++ src/commands/viajantes/perfil.js
@@ -1,18 +1,19 @@
 const Discord = require('discord.js');
 const Canvas = require('canvas');
 const User = require('../../mongoDB/schemas/userschema');
-const MeiButton = require('../../functions/buttonBuilder')
-Canvas.registerFont('./src/assets/fonts/segoepr.ttf', { family: 'Segoe' });
+const MeiButton = require('../../functions/buttonBuilder');
+const abbreviate = require('number-abbreviate');
+Canvas.registerFont('./src/assets/fonts/Roboto-Light.ttf',{ family: 'Roboto' });
 
 
 module.exports = {
   name: 'perfil',
   aliases: ["profile", "p"],
-  description: 'ain bolsonaro',
+  description: '🔮 Veja suas informações em um perfil belo e compacto.',
   category: "members",
   run: async (client, message, args) => {
 
-    const canvas = Canvas.createCanvas(1535, 863);
+    const canvas = Canvas.createCanvas(1440, 720);
     const ctx = canvas.getContext('2d');
 
     const user = message.mentions.users.first() || message.author;
@@ -21,7 +22,7 @@ module.exports = {
     const Avatar = user.avatarURL({ extension: 'png', dynamic: true, size: 2048 });
 
     const userBackground = await Canvas.loadImage(userDB.banner);
-    const background = await Canvas.loadImage('https://i.imgur.com/JHTnTT6.png');
+    const background = await Canvas.loadImage('https://i.imgur.com/XBrxIUq.png');
     const avatar = await Canvas.loadImage(Avatar);
 
     let button = MeiButton([
@@ -32,43 +33,65 @@ module.exports = {
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
   
  
-    ctx.font = '45px Arial'; // User Name
-    ctx.fillStyle = '#9eec94';
-    ctx.fillText(user.username, 500, 113);
-
-    ctx.font = '38px Arial'; // User Id
-    ctx.fillStyle = '#9eec94';
-    ctx.fillText(user.id, 440, 177);
-    
-    ctx.font = '38px Arial'; // User Fragments
-    ctx.fillStyle = '#9eec94';
-    ctx.fillText(`${userDB.bolso.toLocaleString()}`, 610, 250);
-    
-    ctx.font = '45px Arial'; // User Honor
-    ctx.fillStyle = '#9eec94';
-    ctx.fillText(userDB.honor, 1330, 690);
-    
-    ctx.font = '38px Segoe'; // User About
+    ctx.font = '55px Arial'; // User Name
     ctx.fillStyle = '#ffffff';
-    ctx.fillText(userDB.aboutme, 40, 750);
+    ctx.fillText(limitText(user.username, 15), 360, 280);
+
+    ctx.font = '35px Arial'; // User Id
+    ctx.fillStyle = '#757575';
+    ctx.fillText(user.id, 348, 314);
     
-    let Tcirculo = 150; // Avatar and Circle
-    let X = 185;
-    let Y = 170;
-  
-    ctx.arc(X, Y, Tcirculo, 0, Math.PI * 2, true); 
-    ctx.strokeStyle = "#749270"; 
-    ctx.lineWidth = 8; 
+    ctx.font = '30px Arial'; // User Fragments
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText(`${userDB.bolso.toLocaleString()}`, 320, 434);
+    
+    ctx.font = '30px Arial'; // User Mushrooms
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText(`${userDB.mushroom.toLocaleString()}`, 335, 475); 
+    
+    ctx.font = '30px Arial'; // User Honor
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText(abbreviate(userDB.honor), 1270, 60);
+
+
+     
+    function limitText(text, maxLength) {
+      return text.length > maxLength ? text.slice(0, maxLength) + '...' : text;
+    }
+
+    let aboutme =  limitText(userDB.aboutme, 160); // User About
+    let maxChar = 70;
+    let lineHeight = 30; 
+    let textStartX = 35; 
+    let textStartY = 630; 
+    
+    ctx.font = '28px Roboto';
+    ctx.fillStyle = '#ffffff';
+    
+    let lines = [];
+    let currentLine = '';
+    
+    for(let i = 0; i < aboutme.length; i++) {
+      if(i !== 0 && i % maxChar === 0) {
+        lines.push(currentLine);
+        currentLine = '';
+      }
+      currentLine += aboutme[i];
+    }
+    lines.push(currentLine);
+    
+    for(let i = 0; i < lines.length; i++) {
+      ctx.fillText(lines[i], textStartX, textStartY + (i * lineHeight));
+    }
+    
+    let x = 28, y = 70, avatarSize = 320; // User Avatar
+    ctx.beginPath();
+    ctx.arc(x + avatarSize / 2, y + avatarSize / 2, avatarSize / 2, 0, Math.PI * 2, true);
     ctx.closePath();
-    ctx.stroke();
-    ctx.clip(); 
-  
-    let Lavatar = 330;
-    let Aavatar = 330;
-    let xAvatar = X - Lavatar / 2; 
-    let yAvatar = Y - Aavatar / 2; 
-    ctx.drawImage(avatar, xAvatar, yAvatar, Lavatar, Aavatar);
-    
+    ctx.clip();
+    ctx.drawImage(avatar, x, y, avatarSize, avatarSize);
+
+      
     try {
       const Message = await message.reply(client.FormatEmoji(`{e:dance} ${message.author}, estou gerando o perfil.`));
       const profile = new Discord.AttachmentBuilder(canvas.toBuffer(), 'perfil.png');
